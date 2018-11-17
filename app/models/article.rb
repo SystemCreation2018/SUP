673b4f468e1e2186cbad1b3fd5b202992fa396fd
s163024 <s163024@matsu.shimane-u.ac.jp>
2018-11-17 02:33:09 -0500

    add: validate article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
+  
+  validates :title, presence: true
+  validates :title, length: { maximum: 50 }
+  validates :content, presence: true
 end
